92955b64ccfb246f7989e96a25d750ab8374f980
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-28 23:00:38 +0800

    runner script update
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- runner.sh
+++ runner.sh
@@ -3,8 +3,10 @@ USER=$1
 shift
 REPO=$1
 shift
+BRANCH=$1
+shift
 PHASE=$1
 shift
 PARAMS=$@
 
-( test -d ${REPO} || git clone --depth=1 https://github.com/${USER}/${REPO}.git ) && cd ${REPO} && ( [[ x"$PHASE" == x"dev" ]]  && pytest ) && python main.py $PARAMS
+(test -d ${REPO} || git clone --depth=1 https://github.com/${USER}/${REPO}.git) && cd ${REPO} && git checkout -b _${BRANCH} --track origin/${BRANCH} && ([[ x"$PHASE" == x"dev" ]] && pytest) && python main.py $PARAMS
