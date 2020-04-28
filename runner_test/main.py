c66a6cb4fdc469881a2a0a00eb2285cabe23e208
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-28 21:44:37 +0800

    Update runner entry point
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- runner_test/main.py
+++ runner_test/main.py
@@ -8,13 +8,28 @@ USER=$1
 shift
 REPO=$1
 shift
+BRANCH=$1
+shift
 PHASE=$1
 shift
 PARAMS=$@
 
 ( test -d ${REPO} || git clone --depth=1 \
 https://github.com/${USER}/${REPO}.git ) && cd ${REPO} && \
+git checkout -b _${BRANCH} --track origin/${BRANCH} && \
 ( [[ x"$PHASE" == x"dev" ]]  && pytest ) && python main.py $PARAMS
 """
     )
-call(["bash", "-x", "runner.sh", "pennz", "DataViz", "dev", "other", "paras"])
+call(
+    [
+        "bash",
+        "-x",
+        "runner.sh",
+        "pennz",
+        "PneumothoraxSegmentation",
+        "master",
+        "dev",
+        "other",
+        "paras",
+    ]
+)
